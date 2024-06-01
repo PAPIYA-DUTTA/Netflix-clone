efde6aac218e75e9ba77a93603a93be70376fe5e
PAPIYA-DUTTA <papiyad258@gmail.com>
2024-06-01 08:11:53 +0530

    added-app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Netflix-clone/",
   server:{
     port:3000,
   }
